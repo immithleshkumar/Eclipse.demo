97bf3092ef7afdbd621f7a33d81aebc3c618a907
Mithlesh kumar <mithleshkumar852144@gmail.com>
2024-12-01 18:51:17 +0530

    first load the github

--- firstprogram/src/firstprogram/Add.java
+++ firstprogram/src/firstprogram/Add.java
@@ -3,7 +3,10 @@ package firstprogram;
 public class Add {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a=9;
+		int b=67;
+		int sum=a+b;
+		System.out.println(sum);
 
 	}
 
---
+++ firstprogram/src/firstprogram/Substract.java
@@ -0,0 +1,10 @@
+package firstprogram;
+
+public class Substract {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
